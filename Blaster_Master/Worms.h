b67af07b8dfbd0162e6be33a24423bcee6bdcaa2
MinhTran0311 <50918867+MinhTran0311@users.noreply.github.com>
2020-12-31 22:56:23 +0700

    fix worm

--- Blaster_Master/Worms.h
+++ Blaster_Master/Worms.h
@@ -3,11 +3,12 @@
 #include "Jason.h"
 #include "Timer.h"
 #include "Brick.h"
+#include "InjuringBrick.h"
 #include <ctime>
 
 #define WORM_WALKING_SPEED	0.05f
 #define WORM_GRAVITY		0.0002f
-#define WORM_JUMP_SPEED_Y	0.06f	
+#define WORM_CLIMB_SPEED_Y	0.06f	
 
 #define WORM_BBOX_WIDTH 17
 #define WORM_BBOX_HEIGHT 8
@@ -15,12 +16,12 @@
 
 #define WORM_STATE_WALKING	100
 #define WORM_STATE_DIE 200
-#define WORM_STATE_JUMPING 300
+#define WORM_STATE_CLIMB 300
 
 #define WORM_ANI_WALKING 0
 #define WORM_ANI_DIE 1
 
-#define WORM_SITEFOLLOW_PLAYER			100
+#define WORM_SITEFOLLOW_PLAYER			150
 #define WORM_SITEACTIVE_PLAYER			180
 
 #define WORM_MAXHEALTH					1
@@ -30,6 +31,9 @@ class Worm : public Enemy
 {
 	bool isFollow;
 	bool isContainedInLarva;
+	float larvaFloor;
+	int brickHeight;
+
 
 	virtual void GetBoundingBox(float& left, float& top, float& right, float& bottom);
 	virtual void Update(DWORD dt, vector<LPGAMEENTITY>* coObjects=NULL);
