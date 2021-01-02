233debfb8eb0d9995f7e1101d35b8e090c920a81
18520584 <datpham1610@gmail.com>
2021-01-02 20:22:19 +0700

    fix BulletJason shoot Enemies

--- Blaster_Master/Bullet.cpp
+++ Blaster_Master/Bullet.cpp
@@ -52,6 +52,23 @@ void Bullet::HandlePlayerBulletCollision(vector<LPGAMEENTITY>* coObjects)
 			colliable_Objects->push_back(coObjects->at(i));
 	}
 
+	for (UINT i = 0; i < colliable_Objects->size(); i++)
+	{
+		if (this->IsCollidingObject(colliable_Objects->at(i)))
+		{
+			LPGAMEENTITY e = colliable_Objects->at(i);
+			switch (e->GetType())
+			{
+				case ENEMY:
+					e->AddHealth(-dam);
+					this->SetState(BULLET_STATE_ENEMY);
+					break;
+				default:
+					break;
+			}
+		}
+	}
+
 #pragma region collision
 	vector<LPCOLLISIONEVENT> coEvents;
 	vector<LPCOLLISIONEVENT> coEventsResult;
